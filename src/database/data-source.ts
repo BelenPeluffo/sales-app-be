d91ac4185f04c68de58c1187162dee2eab21f78e
Belén Peluffo <belenpeluffolupiano@gmail.com>
2026-02-28 17:12:56 -0300

    feat: instala paquetes y crea data source

---
+++ src/database/data-source.ts
@@ -0,0 +1,12 @@
+export const AppDataSource = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/database/migrations/*.js'],
+  logging: false,
+};
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
