1e23186d911f803f97b7e238114c793aadcfaeed
Belén Peluffo <belenpeluffolupiano@gmail.com>
2026-03-01 02:47:44 -0300

    fix (deploy): implementa condicionado de credenciales para usar unas en development y otras en producción

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -5,14 +5,21 @@ import * as path from 'path';
 const isProduction = process.env.NODE_ENV === 'production';
 const extension = isProduction ? 'js' : 'ts';
 
+const dbCreds = isProduction
+  ? { url: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }
+  : {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl:
+        process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+    };
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+  ...dbCreds,
   entities: [path.join(__dirname, `../modules/**/*.entity.${extension}`)],
   migrations: [path.join(__dirname, `./migrations/*.${extension}`)],
   logging: false,
